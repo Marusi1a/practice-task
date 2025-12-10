26cf04cc649c8bc741fb926d71c50a83d667bace
Marusi1a <marusiachasodiyka@gmail.com>
2025-12-10 18:44:52 +0200

    Update task_4.py
    Програма виконує завдання та без оптимізацій

--- task_4.py
+++ task_4.py
@@ -1,45 +1,95 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urljoin
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
-}
-wiki_page = "https://en.wikipedia.org/wiki/Isaac_Newton"
-def get_first_link(page_url):
-    response = requests.get(page_url, headers=headers)
-    response.raise_for_status()
+wiki_page = input("Put first wiki link: ")
+keywords = ["Hitler", "dictator", "war"]
+target = "https://en.wikipedia.org/wiki/Adolf_Hitler"
 
-    soup = BeautifulSoup(response.text, "html.parser")
-    content = soup.find("div", id="mw-content-text")
 
-    # пошук першого змістовного лінку
-    for p in content.find_all("p"):
-        links = p.find_all("a", href=True)
-        if not links:
+# ім'я користувача щоб уникнути блокувань на вікіпедії, бо та не пускає ботів
+def get_html(url):
+    headers = {"User-Agent": "Mozilla/5.0"}
+    r = requests.get(url, headers=headers)
+    r.raise_for_status()
+    return r.text
+
+def get_first_relevant_link(url):
+    html = get_html(url)
+    soup = BeautifulSoup(html, "html.parser")
+
+    content = soup.find("div", {"id": "mw-content-text"})
+    if not content:
+        return None
+
+    links = content.find_all("a", href=True)
+
+    # Зберігаємо перший лінк та окремі ключові лінки
+    first_link = None
+    uni_link = None
+    school_link = None
+    math_link = None
+
+    for a in links:
+        href = a.get("href")
+        if not href or not href.startswith("/wiki/"):
             continue
 
-        for tag in links:
-            href = tag["href"]
+        # ігнорувати службові сторінки
+        if ":" in href:
+            continue
+        href_lower = href.lower()
+        if "disambiguation" in href_lower:
+            continue
 
-            # пропускаємо службові лінки
-            if href.startswith("#"):
-                continue
-            if href.startswith("/wiki/File:"):
-                continue
+        # перший звичайний лінк
+        if first_link is None:
+            first_link = href
 
-            # повертаємо повний URL
-            return "https://en.wikipedia.org" + href
+        text = a.get_text().lower()
 
-    return None
+        # Пріоритетний пошук
+        if ("Hitler" in href_lower or "hitler" in text) and uni_link is None:
+            uni_link = href
+
+        elif ("Dictator" in href_lower or "dictator" in text) and school_link is None:
+            school_link = href
+
+        elif ("War" in href_lower or "war" in text) and math_link is None:
+            math_link = href
+
+    # Вибір за пріоритетами
+    if uni_link:
+        return uni_link
+    if school_link:
+        return school_link
+    if math_link:
+        return math_link
+
+    return first_link
+
+
+
 
 #цикл для пошуку лінків
-for i in range(6):
-    print(f"Крок {i+1}: {wiki_page}")
+current =  wiki_page
+found = False
 
-    next_link = get_first_link(wiki_page)
+for step in range(6):
+    print(f"Крок {step + 1}: {current}")
 
-    if not next_link:
-        print("Перший лінк не знайдено. Зупиняюсь.")
+    if target in current:
+        found = True
         break
-    wiki_page = next_link
 
+    next_link = get_first_relevant_link(current)
+    if not next_link:
+        break
+
+    current = urljoin("https://en.wikipedia.org", next_link)
+
+# Виведення результату
+if found or (target in current):
+    print("Hitler found!", current)
+else:
+    print("Hitler not found")
